534a670c7faa458fa15e6dacf84954b194f57445
desarrollo <desarrollo@academicos.cesde.edu.co>
2023-05-13 09:54:00 -0500

    continuacion ejercicio

--- Ejercicio.py
+++ Ejercicio.py
@@ -20,7 +20,51 @@ def listaprimos(numeros):
     return primos
 print(listaprimos([26, 33, 4, 15, 63, 17, 8, 98, 10]))
 
-#3. Crear una funcion que,al reciir una lista de numeros, devuleva el que mas se repite y cuantas veces lo hace. Si hay mas de un "mas repetido", que devuelva cualquiera
+#3. Crear una función que convierta entre grados Celsius, Farenheit y Kelvin Convertir a farenheit : (°C × 9/5) + 32 = °F Convertir a Kelvin: °C + 273.15 = °K Debe recibir 3 parámetros: el valor, la medida de origen y la medida de destino
+
+def convertir_temperatura(valor, medida_origen, medida_destino):
+    if medida_origen == 'Celsius':
+        if medida_destino == 'Fahrenheit':
+            return (valor * 9/5) + 32
+        elif medida_destino == 'Kelvin':
+            return valor + 273.15
+        else:
+            return valor  # Si la medida de origen y destino es la misma, no se realiza ninguna conversión
+    elif medida_origen == 'Fahrenheit':
+        if medida_destino == 'Celsius':
+            return (valor - 32) * 5/9
+        elif medida_destino == 'Kelvin':
+            return (valor - 32) * 5/9 + 273.15
+        else:
+            return valor
+    elif medida_origen == 'Kelvin':
+        if medida_destino == 'Celsius':
+            return valor - 273.15
+        elif medida_destino == 'Fahrenheit':
+            return (valor - 273.15) * 9/5 + 32
+        else:
+            return valor
+    else:
+        return valor  # Si la medida de origen y destino no es válida, no se realiza ninguna conversión
+
+
+#4.Iterando una lista con los tres valores posibles de temperatura que recibe la función del punto 5, hacer un print para cada combinación de los mismos:
+
+temperaturas = [25, 0, 100]  # Ejemplo de lista con tres valores de temperatura
+
+unidades = ['Celsius', 'Fahrenheit', 'Kelvin']  # Lista de las tres unidades posibles
+
+for temperatura in temperaturas:
+    for origen in unidades:
+        for destino in unidades:
+            if origen != destino:  # Evitar imprimir conversiones de la misma unidad
+                resultado = convertir_temperatura(temperatura, origen, destino)
+                print(f"{temperatura} grados {origen} son equivalentes a {resultado} grados {destino}")
+
+
+
+
+
 
 
 
---
+++ evaluacion.py
@@ -0,0 +1,41 @@
+"""-80
+"hola"
+(a,b,c)
+True
+100
+
+lista = [10, 3.14, "python"]
+lista.append("oop")
+print(lista)
+
+colores_primarios = ["amarillo", "azul", "rojo"]
+colores_primarios.extend(["verde", "naranja", "violeta", ["negro"]])
+print(colores_primarios)
+
+n = 5
+if n>0:
+    print("numero positivo")
+elif n == 0:
+    print("es cero")
+else:
+    print("numero positivo")
+
+x = 0
+while x < 5:
+    x+= 1
+    if x == 3:
+        continue
+    print(x)
+
+for num in range (0,5):
+    print(num)
+
+a=1
+print(a+b)
+
+paises = ("alemania", "belgica", "dinamarca", "colombia", "mexico", "peru")
+esto es una tupla
+
+num = range(8, 16, 2)
+for n in num:
+    print(n)"""
